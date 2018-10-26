50ab793fad474920ea39c92d24d58c863ffcd994
Jacco de Vries <jdevries@nikhef.nl>
2018-10-26 15:32:14 +0200

    added copy script

---
+++ datafiles/scripts/copy_ganga_cernbox.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+jobID=4
+
+gangadir=/data/bfys/jdevries/gangadir/workspace/jdevries/LocalXML/${jobID}
+cernboxdir=/eos/user/j/jadevrie/LcAnalysis_Simon/datafiles/ganga/${jobID}
+
+echo "Will copy job ${jobID} which has the following subjobs:"
+du -lh --max-depth 1 $gangadir
+
+#scp -rCv $gangadir lxplus:$cernboxdir 
+rsync -vr --ignore-existing $gangadir lxplus:$cernboxdir
+
