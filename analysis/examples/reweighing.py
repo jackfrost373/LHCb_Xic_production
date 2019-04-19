29c3c9e8d5dbc15968cb5dbed97a09f00ecf210d
Jacco de Vries <jdevries@nikhef.nl>
2019-04-19 12:58:41 +0200

    improved readibility of kWeights script, renamed to reweighing.py

--- analysis/examples/reweighing.py
+++ analysis/examples/reweighing.py
@@ -17,10 +17,10 @@ simtreename = datatreename
 
 variables = ["lcplus_P", "lcplus_ETA"]
 
-#binning = { "lcplus_P" : [5000, 20000, 30000, 35000, 40000, 45000, 50000, 60000, 70000, 80000, 100000, 150000, 200000, 300000, 450000] ,
-#          "lcplus_ETA" : [2.0, 2.5, 2.7, 2.9, 3.1, 3.3, 3.5, 4.0, 4.5, 5.5] }
-binning = { "lcplus_P" : [5000, 30000, 40000, 50000, 60000, 80000, 150000, 450000] ,
-          "lcplus_ETA" : [2.0, 2.9, 3.3, 4.0, 5.5] }
+binning = { "lcplus_P" : [5000, 20000, 30000, 35000, 40000, 45000, 50000, 60000, 70000, 80000, 100000, 150000, 200000, 300000, 450000] ,
+          "lcplus_ETA" : [2.0, 2.5, 2.7, 2.9, 3.1, 3.3, 3.5, 4.0, 4.5, 5.5] }
+#binning = { "lcplus_P" : [5000, 30000, 40000, 60000, 90000, 450000] ,
+#          "lcplus_ETA" : [2.0, 2.7, 3.1, 3.7, 5.5] }
 
 cuts = { "data" : "1==1",
           "sim" : "1==1" }  # sim should include MCTruth
@@ -29,7 +29,9 @@ weights = { "data" : "1==1",  # data should contain sweight (but add friendTree 
              "sim" : "1==1" }
 
 outputdir = "./output/"
-showplots = True
+
+makeplots = True
+nbins = 100 # bins for regular drawing
 
 
 
@@ -38,8 +40,7 @@ showplots = True
 
 # set some stuff
 ROOT.gStyle.SetOptStat(0) # remove hist info box
-#if not showplots : ROOT.gROOT.SetBatch(True) 
-nbins = 100 # bins for regular drawing
+if not makeplots : ROOT.gROOT.SetBatch(True) # prevent graphical output
 ranges = [ [binning[variables[0]][0], binning[variables[0]][-1]],
            [binning[variables[1]][0], binning[variables[1]][-1]] ]
 
@@ -49,12 +50,16 @@ simfile  = ROOT.TFile.Open(simfile_loc)
 datatree = datafile.Get(datatreename)
 simtree  = simfile.Get(simtreename)
 
-# make dictionaries for easy access/looping
+# Make global dictionaries for easy access/looping.
+# This will also prevent objects from going out-of-scope when created in function calls.
 treedict = { "data" : datatree,
              "sim"  : simtree }
-colourdict = { "data" : 9,         # blue
-               "sim"  : 46,        # red
-               "kweighed" : 28 }  # brown
+colourdict = { "data" : 9,         # 9=blue
+               "sim"  : 28,        # 28=brown
+               "kweighed" : 46 }   # 46=red
+styledict  = { "data" : 1,         # 1=solid
+               "sim"  : 1,         # 1=solid
+               "kweighed" : 7 }    # 7=small dashed
 histdict = {}
 canvasdict = {}
 
@@ -66,36 +71,37 @@ canvasdict = {}
 #############################
 
 
-lines = []
-def drawBinningLines(binning=binning, variables=variables, lines=lines) :
-  # Draw red lines at the 2D bin edges
-  if(lines == []) :
-    xmax = max(binning[variables[0]])
-    xmin = min(binning[variables[0]])
-    for y in binning[variables[1]] :
-      lines += [ ROOT.TLine(xmin, y, xmax, y) ]
-    ymax = max(binning[variables[1]])
-    ymin = min(binning[variables[1]])
-    for x in binning[variables[0]] :
-      lines += [ ROOT.TLine(x, ymin, x, ymax) ]
+def makeBinningLines(binning=binning, variables=variables) :
+  # make red lines at the 2D bin edges
+  lines = []
+  xmax = max(binning[variables[0]])
+  xmin = min(binning[variables[0]])
+  for y in binning[variables[1]] :
+    lines += [ ROOT.TLine(xmin, y, xmax, y) ]
+  ymax = max(binning[variables[1]])
+  ymin = min(binning[variables[1]])
+  for x in binning[variables[0]] :
+    lines += [ ROOT.TLine(x, ymin, x, ymax) ]
 
   for line in lines :
     line.SetLineColor(2) # red
-    line.Draw("same")
+
+  return lines 
 
 
 
 def makeKweightsTable(treedict=treedict, variables=variables, binning=binning, cuts=cuts, weights=weights) : 
   # Make a look-up-table (=2D hist) for the kinematic weights
+
   print("Making and dividing histograms for kWeights")
  
+  # Create 2D histograms with variable-sized bin widths
   xbins = array('d', binning[variables[0]])
   ybins = array('d', binning[variables[1]])
-  nbinsx  = len(xbins)
-  nbinsy  = len(ybins)
-  binnedHist2D_data = ROOT.TH2F("binnedHist2D_data", "binnedHist2D_data", nbinsx-1, xbins, nbinsy-1, ybins )
-  binnedHist2D_sim  = ROOT.TH2F("binnedHist2D_sim" , "binnedHist2D_sim" , nbinsx-1, xbins, nbinsy-1, ybins )
+  binnedHist2D_data = ROOT.TH2F("binnedHist2D_data", "binnedHist2D_data", len(xbins)-1, xbins, len(ybins)-1, ybins )
+  binnedHist2D_sim  = ROOT.TH2F("binnedHist2D_sim" , "binnedHist2D_sim" , len(xbins)-1, xbins, len(ybins)-1, ybins )
 
+  # Fill histograms with TTree::Draw
   for datatype in ["data","sim"] :
     treedict[datatype].Draw("{0}:{1}>>+binnedHist2D_{2}".format(variables[1],variables[0], datatype),
       "({0})*{1}".format(cuts[datatype], weights[datatype]) )
@@ -104,21 +110,25 @@ def makeKweightsTable(treedict=treedict, variables=variables, binning=binning, c
     hist.Sumw2() # set errors as sqrt(sum(weights^2)) (not sqrt(N))
     hist.Scale( 1./hist.Integral() )  # normalize histogram
 
-  # the big trick
+  # The big trick. Idea: [sim] x [kWeight] = [data]
+  #  --> [kWeight] = [data] / [sim]
   binnedHist2D_data.Divide( binnedHist2D_sim )
 
+  # binnedHist2D_data is now the kWeight histogram.
   binnedHist2D_data.SetName("kWeights")
   binnedHist2D_data.SetTitle("kWeights")
-  binnedHist2D_data.GetXaxis().SetTitle(variables[1])
-  binnedHist2D_data.GetYaxis().SetTitle(variables[0])
+  binnedHist2D_data.GetXaxis().SetTitle(variables[0])
+  binnedHist2D_data.GetYaxis().SetTitle(variables[1])
 
   return binnedHist2D_data
 
 
 
+
 def makeKweightsFriendTree(kweightsTable, simtree=simtree, variables=variables, 
     outputfilename=outputdir+"kWeight_kwTree.root", cutoff = 5.) : 
-  # Loop over tree and look up the kWeight for each entry
+  # Loop over tree and look up the kWeight for each entry.
+  #  kWeights larger than cutoff will be set to cutoff value.
 
   # TTrees directly access memory, so we define pointers.
   var1    = array( 'd', [0] )
@@ -128,12 +138,16 @@ def makeKweightsFriendTree(kweightsTable, simtree=simtree, variables=variables,
 
   nEntries = int(simtree.GetEntries())
 
-  # first get naive kWeights
+  # first get naive kWeights, by looping over tree
   kwlist = []
   print("Getting kWeights for {0} entries...".format(nEntries))
   for i in range(nEntries) :
     if(i%10000==0) : print("{0:.2f} %".format(float(i)/nEntries*100.))
+
+    # Get tree entry to fill var1 and var2
     simtree.GetEntry(i)
+
+    # Find the bin content in the kWeighsTable, corresponding to the right var1, var2 bin
     kw = kweightsTable.GetBinContent( kweightsTable.FindBin( var1[0] , var2[0] ) )
     if( kw > cutoff ) : kw = cutoff
     kwlist += [kw]
@@ -141,19 +155,17 @@ def makeKweightsFriendTree(kweightsTable, simtree=simtree, variables=variables,
   # calculate normalisation
   kwtotal  = sum(kwlist)
   kw2total = sum( [i**2 for i in kwlist] )
-  if(kw2total==0) :
-    print("Error: kweight sum is zero. Something must have gone wrong!")
-    return
+  if(kw2total==0) : print("Error: kweight sum is zero. Something must have gone wrong!")
   norm = float(kwtotal) / kw2total
   print("--> kWeight normalisation is {0}".format(norm))
  
-  # open kweights file
+  # open kweights file to save our kWeights
   kwfile = ROOT.TFile.Open(outputfilename, "RECREATE")
   kwtree = ROOT.TTree("kwTree","kwTree")
   kweight = array( 'f', [0] )
   kwtree.Branch('kweight', kweight, 'kweight/F')
 
-  # normalize and write
+  # Normalize kWeights and write
   print("Normalizing kWeights...")
   for i in range(nEntries) :
     if(i%10000==0) : print("{0:.2f} %".format(float(i)/nEntries*100.))
@@ -170,55 +182,62 @@ def makeKweightsFriendTree(kweightsTable, simtree=simtree, variables=variables,
     
 
 ##############################
-# Plot to show what distributions we're dealing with
+# First, plot to show what distributions we're dealing with
 ##############################
 
-c1 = ROOT.TCanvas()
+c1 = ROOT.TCanvas() # Dummy canvas for TTree::Draw calls
 
-if(showplots) :
+if(makeplots) :
   
   print("Making histograms for plotting")
 
   # Make the 1D histograms
   for i in range(2) :
+    vari = "var{0}".format(i)
+    var = variables[i]
     for datatype in ["data","sim"] :
-      histname = "{0}_1D_var{1}".format(datatype, i)
-      treedict[datatype].Draw("{0}>>{1}({2},{3},{4})".format(variables[i], histname,
+      histname = datatype+"_1D_"+vari
+      treedict[datatype].Draw("{0}>>{1}({2},{3},{4})".format(var, histname,
         nbins, ranges[i][0], ranges[i][1]), 
         "({0})*{1}".format(cuts[datatype], weights[datatype]) )
       histdict[ histname ] = ROOT.gDirectory.Get( histname )
-      histdict[ histname ].SetTitle(histname)
-      histdict[ histname ].GetXaxis().SetTitle(variables[i])
+      histdict[ histname ].SetTitle("")
+      histdict[ histname ].GetXaxis().SetTitle(var)
       histdict[ histname ].SetLineWidth(2)
+      histdict[ histname ].SetLineStyle( styledict[  datatype ] )
       histdict[ histname ].SetLineColor( colourdict[ datatype ] )
 
   # Make the 2D histograms
   for datatype in ["data","sim"] :
-    histname = "{0}_2D".format(datatype)
+    histname = datatype+"_2D"
     treedict[datatype].Draw("{0}:{1}>>{2}({3},{4},{5},{6},{7},{8})".format(variables[1],variables[0], histname,
       nbins, ranges[0][0], ranges[0][1], nbins, ranges[1][0], ranges[1][1] ),
       "({0})*{1}".format(cuts[datatype], weights[datatype]) )
     histdict[ histname ] = ROOT.gDirectory.Get( histname )
     histdict[ histname ].SetTitle(histname)
-    histdict[ histname ].GetXaxis().SetTitle(variables[1])
-    histdict[ histname ].GetYaxis().SetTitle(variables[0])
+    histdict[ histname ].GetXaxis().SetTitle(variables[0])
+    histdict[ histname ].GetYaxis().SetTitle(variables[1])
 
   print("Drawing histograms")
 
   # Draw the 1D histograms
   for i in range(2) :
-    canvasdict[ "c_1D_var{0}".format(i) ] = ROOT.TCanvas("c_1D_var{0}".format(i))
-    histdict[ "data_1D_var{0}".format(i) ].DrawNormalized()
-    histdict[  "sim_1D_var{0}".format(i) ].DrawNormalized("same")
-    canvasdict[ "c_1D_var{0}".format(i) ].Update()
+    vari = "var{0}".format(i)
+    cname = "c_1D_"+vari
+    canvasdict[ cname ] = ROOT.TCanvas(cname)
+    histdict[ "data_1D_"+vari ].DrawNormalized()
+    histdict[  "sim_1D_"+vari ].DrawNormalized("same")
+    canvasdict[ cname ].Update()
 
   # Draw the 2D histograms
+  binedgelines = makeBinningLines(binning, variables)
   for datatype in ["data","sim"] :
-    canvasdict[ "c_2D_{0}".format(datatype) ] = ROOT.TCanvas("c_2D_{0}".format(datatype))
-    histdict[ "{0}_2D".format(datatype) ].Draw("colz")
-    drawBinningLines()
-    canvasdict[ "c_2D_{0}".format(datatype) ].Update()
-    canvasdict[ "c_2D_{0}".format(datatype) ].SaveAs(outputdir+"2D_{0}.pdf".format(datatype))
+    cname = "c_2D_"+datatype
+    canvasdict[ cname ] = ROOT.TCanvas(cname)
+    histdict[ datatype+"_2D" ].Draw("colz")
+    for line in binedgelines : line.Draw("same")
+    canvasdict[ cname ].Update()
+    canvasdict[ cname ].SaveAs(outputdir+"kWeights_2D_"+datatype+".pdf")
 
 
 
@@ -226,46 +245,69 @@ if(showplots) :
 
 
 ##############################
-# Make kinematic weights and draw resulting weighed distributions
+# Make kinematic weights, save as friendtree.
+# This is the core business of this script.
 ##############################
 
 kwfilename = outputdir + "kWeight_kwTree.root"
+c1.cd()
 
 # Make kWeight table
-c1.cd()
-kweightTable = makeKweightsTable() 
+kweightTable = makeKweightsTable(treedict, variables, binning, cuts, weights) 
 
-if(showplots) :
+# Make kWeights friend tree for simtree
+makeKweightsFriendTree(kweightTable, simtree, variables, kwfilename)
+
+# Add friend tree to simulation
+simtree.AddFriend("kwTree",kwfilename)
+
+
+
+
+
+##############################
+# Show off the effect of our kWeights
+##############################
+
+if(makeplots) :
+ 
+  # Draw kWeights
   c_2D_kWeights = ROOT.TCanvas("c_2D_kWeights")
   kweightTable.Draw("colz")
   c_2D_kWeights.SaveAs(outputdir+"kWeights.pdf")
 
-
-# Make kWeights friend tree for simtree
-makeKweightsFriendTree(kweightTable)
-
-# Add friendtree
-simtree.AddFriend("kwTree",kwfilename)
-
-
-# Draw new kinematic-weighed distributions
-if(showplots) :
-
   c1.cd()
+  legendas = {} # make dict for persistency
+
+  # Draw reweighed sim distributions in the same 1D histograms
   for i in range(2) :
-    histname = "kweighedsim_1D_var{0}".format(i)
-    
-    simtree.Draw("{0}>>{1}({2},{3},{4})".format(variables[i], histname, nbins, ranges[i][0], ranges[i][1]), 
+    vari = "var{0}".format(i)
+    var = variables[i]
+    histname = "kweighedsim_1D_"+vari
+   
+    # Draw reweighed sim distribution
+    simtree.Draw("{0}>>{1}({2},{3},{4})".format(var, histname, nbins, ranges[i][0], ranges[i][1]), 
       "({0})*({1})*kwTree.kweight".format(cuts["sim"], weights["sim"]) )
 
+    # Set some properties
     histdict[ histname ] = ROOT.gDirectory.Get( histname )
     histdict[ histname ].SetTitle(histname)
     histdict[ histname ].GetXaxis().SetTitle(variables[i])
     histdict[ histname ].SetLineWidth(2)
-    histdict[ histname ].SetLineStyle(9)
+    histdict[ histname ].SetLineStyle( styledict[  "kweighed" ] )
     histdict[ histname ].SetLineColor( colourdict[ "kweighed" ] )
-    
-    canvasdict[ "c_1D_var{0}".format(i) ].cd()
-    histdict[ histname ] .DrawNormalized("same")
-    canvasdict[ "c_1D_var{0}".format(i) ].Update()
-    canvasdict[ "c_1D_var{0}".format(i) ].SaveAs(outputdir+"1D_var{0}.pdf".format(i))
+  
+    # Make legend
+    legendas[vari] = ROOT.TLegend(0.76,0.77,0.9,0.9)
+    legendas[vari].AddEntry( histdict["data_1D_"+vari],         "data",     "l")
+    legendas[vari].AddEntry( histdict["sim_1D_"+vari ],         "sim",      "l")
+    legendas[vari].AddEntry( histdict["kweighedsim_1D_"+vari],  "rew. sim", "l")
+  
+    # Draw and save
+    cname = "c_1D_"+vari
+    canvasdict[ cname ].cd()
+    histdict[ histname ].DrawNormalized("histsame")
+    legendas[vari].Draw("same")
+    canvasdict[ cname ].Update()
+    canvasdict[ cname ].SaveAs(outputdir+"kWeights_1D_"+vari+".pdf")
+
