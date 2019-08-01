7327668e69c204ad9ce57d1731410532b4a9bad8
Simon Calo <simple.simon@hotmail.it>
2019-08-01 12:29:12 +0200

    missing comma issue fixed

--- analysis/Scripts/tuples_prep_scripts/SetBranch.py
+++ analysis/Scripts/tuples_prep_scripts/SetBranch.py
@@ -5,7 +5,7 @@ from ROOT import TChain, TFile
 
 def setBranch_funct (root_file):
 
-    useful_vars = ["lcplus_MM", "lcplus_P", "lcplus_PT", "lcplus_ETA", "lcplus_RAPIDITY", "lcplus_TIP", "lcplus_IPCHI2_OWNPV" "lcplus_L0_Global_TOS", "lcplus_Hlt1Global_TOS", "lcplus_Hlt2Global_TOS", "lcplus_Hlt1Phys_TOS", "lcplus_Hlt2Phys_TOS", "lcplus_Hlt1TrackAllL0Decision_TOS", "lcplus_L0HadronDecision_TOS", "lcplus_Hlt2CharmHadD2HHHDecision_TOS", "pplus_P", "pplus_PT", "pplus_RAPIDITY", "pplus_ETA", "piplus_P", "piplus_PT", "piplus_RAPIDITY", "piplus_ETA", "kminus_P", "kminus_PT", "kminus_RAPIDITY", "kminus_ETA"] # list of variables kept in the tree
+    useful_vars = ["lcplus_MM", "lcplus_P", "lcplus_PT", "lcplus_ETA", "lcplus_RAPIDITY", "lcplus_TIP", "lcplus_IPCHI2_OWNPV", "lcplus_L0_Global_TOS", "lcplus_Hlt1Global_TOS", "lcplus_Hlt2Global_TOS", "lcplus_Hlt1Phys_TOS", "lcplus_Hlt2Phys_TOS", "lcplus_Hlt1TrackAllL0Decision_TOS", "lcplus_L0HadronDecision_TOS", "lcplus_Hlt2CharmHadD2HHHDecision_TOS", "pplus_P", "pplus_PT", "pplus_RAPIDITY", "pplus_ETA", "piplus_P", "piplus_PT", "piplus_RAPIDITY", "piplus_ETA", "kminus_P", "kminus_PT", "kminus_RAPIDITY", "kminus_ETA"] # list of variables kept in the tree
 
 
     tfile = TFile.Open(root_file, "READONLY")  #These 2 lines depend on the type of file fed into the function
