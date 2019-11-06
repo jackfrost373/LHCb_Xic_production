3992f0c5649c7a72e3bc2f408b331ca53a340963
Jonastjep <jonas@tjepkema.com>
2019-11-06 11:10:21 +0100

    Latest version of the library with a second function which only takes the path of file and parses information from it

--- analysis/Scripts/MassFitting/fittingDict.py
+++ analysis/Scripts/MassFitting/fittingDict.py
@@ -17,6 +17,24 @@ fittingDict = {
 				"cb_alpha_range" : [1.0,0.0,5.0],
 				"cb_n_range" : [1.0,0.0,5.0],
 				"cb_normalisation_factor" : 5
+			},
+			#This is just an example
+			"2011_MagDown_Xic_splitfile_y2.5-3.0_pt4000-5000.root" : {
+				"mass_range" : [2420, 2520],
+				"peak_range" : [2469, 2450, 2485],
+				
+				"normalisation_factor" : 1,
+				"gauss_normalisation_factor" : 2,
+				"exponential_normalisation_factor" : 1,
+				
+				"exponential_range" : [-0.0002, -2, 0],
+				
+				"width_range" : [6, 0, 25],
+				
+				"cb_width_range" : [6, 1, 25],
+				"cb_alpha_range" : [1.0,0.0,5.0],
+				"cb_n_range" : [1.0,0.0,5.0],
+				"cb_normalisation_factor" : 5
 			}
 		},
 			
