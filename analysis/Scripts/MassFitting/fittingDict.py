fe5b47f854f47097b0ff0cbeb5ff92bf396d0496
Jonastjep <jonas@tjepkema.com>
2019-11-27 09:40:52 +0100

    Updated version of the dict with improved starting parameters for fitting

--- analysis/Scripts/MassFitting/fittingDict.py
+++ analysis/Scripts/MassFitting/fittingDict.py
@@ -5,56 +5,79 @@ fittingDict = {
 				"mass_range" : [2420, 2520],
 				"peak_range" : [2469, 2450, 2485],
 				
-				"normalisation_factor" : 1,
-				"gauss_normalisation_factor" : 2,
-				"exponential_normalisation_factor" : 1,
+				"normalisation_factor" : 0.1,
+				"exponential_normalisation_factor" : 0.05,
 				
-				"exponential_range" : [-0.0002, -2, 0],
+				"exponential_range" : [-0.02, -2, 0],
 				
 				"width_range" : [6, 0, 25],
 				
 				"cb_width_range" : [6, 1, 25],
-				"cb_alpha_range" : [1.0,0.0,5.0],
-				"cb_n_range" : [1.0,0.0,5.0],
-				"cb_normalisation_factor" : 5
+				"cb_alpha_range" : [24,0.0,25.0],
+				"cb_n_range" : [9.0,0.0,10.0]
 			},
-			#This is just an example
-			"2011_MagDown_Xic_splitfile_y2.5-3.0_pt4000-5000.root" : {
+			"2011_MagDown_Xic_bin_y2.0-2.5_pt5000-6000.root" : {
 				"mass_range" : [2420, 2520],
 				"peak_range" : [2469, 2450, 2485],
 				
-				"normalisation_factor" : 1,
-				"gauss_normalisation_factor" : 2,
-				"exponential_normalisation_factor" : 1,
+				"normalisation_factor" : 0.1,
+				"exponential_normalisation_factor" : 0.05,
 				
-				"exponential_range" : [-0.0002, -2, 0],
+				"exponential_range" : [-0.02, -2, 0],
 				
 				"width_range" : [6, 0, 25],
 				
 				"cb_width_range" : [6, 1, 25],
-				"cb_alpha_range" : [1.0,0.0,5.0],
-				"cb_n_range" : [1.0,0.0,5.0],
-				"cb_normalisation_factor" : 5
+				"cb_alpha_range" : [24,0.0,25.0],
+				"cb_n_range" : [9.0,0.0,10.0]
 			}
 		},
 			
 		"Lc" : {
 			"general" : {
 				"mass_range" : [2240, 2340],
-				"peak_range" : [2289,2260,2320],
+				"peak_range" : [2290,2260,2320],
 				
-				"normalisation_factor" : 6,
-				"gauss_normalisation_factor" : 10,
-				"exponential_normalisation_factor" : 1,
+				"normalisation_factor" : 0.005,
+				"exponential_normalisation_factor" : 0.0001,
 				
-				"exponential_range" : [-0.001, -0.5, 0.5],
+				"exponential_range" : [-0.001, -0.5, 0.1],
 				
 				"width_range" : [6,4,20],
 				
 				"cb_width_range" : [17,8,50],
-				"cb_alpha_range" : [1.0,0.0,5.0],
-				"cb_n_range" : [1.0,0.0,5.0],
-				"cb_normalisation_factor" : 10
+				"cb_alpha_range" : [1.0,0.0,20.0],
+				"cb_n_range" : [1.0,0.0,3.0],
+			},
+			"2011_MagDown_Lc_bin_y2.0-2.5_pt5000-6000.root" : {
+				"mass_range" : [2240, 2340],
+				"peak_range" : [2290,2260,2320],
+				
+				"normalisation_factor" : 0.005,
+				"exponential_normalisation_factor" : 0.0001,
+				
+				"exponential_range" : [-0.2, -0.5, 0.1],
+				
+				"width_range" : [6,4,20],
+				
+				"cb_width_range" : [17,8,50],
+				"cb_alpha_range" : [19.0,0.0,20.0],
+				"cb_n_range" : [9.0,0.0,10.0],
+			},
+			"2011_MagDown_Lc_bin_y2.0-2.5_pt3000-4000.root" : {
+				"mass_range" : [2240, 2340],
+				"peak_range" : [2290,2260,2320],
+				
+				"normalisation_factor" : 0.005,
+				"exponential_normalisation_factor" : 0.0001,
+				
+				"exponential_range" : [-0.001, -0.5, 0.1],
+				
+				"width_range" : [6,4,20],
+				
+				"cb_width_range" : [17,8,50],
+				"cb_alpha_range" : [1.0,0.0,20.0],
+				"cb_n_range" : [1.0,0.0,3.0],
 			}
 		}
 	},
