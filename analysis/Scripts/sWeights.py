908163a3449f636b9242adf1f99a55de44201754
Chris Pawley <cpawley@users.noreply.github.com>
2019-12-01 13:27:28 +0100

    Update sWeights.py

--- analysis/Scripts/sWeights.py
+++ analysis/Scripts/sWeights.py
@@ -1,7 +1,3 @@
-;; This buffer is for text that is not saved, and for Lisp evaluation.
-;; To create a file, visit it with C-x C-f and enter text in its buffer.
-
-
 #################
 # Example of sWeight / sPlot using RooStats,
 # as per https://arxiv.org/abs/physics/0402083
