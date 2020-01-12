265d7dd1e8c9587f4f529daf5e98ea682c9bb00e
Simon Calo <simple.simon@hotmail.it>
2020-01-12 15:06:38 +0100

    Fixed the tuple prep scripts to accomodate both double and single differential. Pt bin now starts from 3.2

--- analysis/Scripts/tuples_prep_scripts/SplitScript.py
+++ analysis/Scripts/tuples_prep_scripts/SplitScript.py
@@ -17,6 +17,7 @@ def split_in_bins_n_save (root_file, saving_dir, run, mother_particle = "Lc"):
     
     os.mkdir(saving_dir + "ybins/")
     os.mkdir(saving_dir + "ptbins/")
+    os.mkdir(saving_dir + "y_ptbins/")
     
     tree = root_file
     for particle in particles:
@@ -27,8 +28,12 @@ def split_in_bins_n_save (root_file, saving_dir, run, mother_particle = "Lc"):
         for ybin in ybins:
             ycuts = "lcplus_RAPIDITY >= {0} && lcplus_RAPIDITY < {1}".format(ybin[0], ybin[1])
             allcuts = " {0} && {1}".format(ycuts, mass_cuts)
-            Strip.strip_n_save(0,0, allcuts, "", saving_dir + "ybins/" + particle, extra_variables = [""], bins = True, y =True, ybin = ybin, tree = tree)
-        for ptbin in ptbins:
-            ptcuts = "lcplus_PT >= {0} && lcplus_PT < {1}".format(ptbin[0], ptbin[1])
-            allcuts = " {0} && {1}".format(ptcuts, mass_cuts)
-            Strip.strip_n_save(0,0, allcuts, "", saving_dir + "ptbins/" + particle, y_2017=False, extra_variables = [""], bins = True, y =False, ptbin = ptbin, tree = tree)
+            Strip.strip_n_save(0,0, allcuts, "", saving_dir + "ybins/" + particle + "_ybin_{0}-{1}.root".format(ybin[0], ybin[1]), extra_variables = [""], bins = True, tree = tree)
+            for ptbin in ptbins:
+                ptcuts = "lcplus_PT >= {0} && lcplus_PT < {1}".format(ptbin[0], ptbin[1])
+                if (ybin[0] == 2.0):
+                    allcuts = " {0} && {1}".format(ptcuts, mass_cuts)
+                    Strip.strip_n_save(0,0, allcuts, "", saving_dir + "ptbins/" + particle + "_ptbin_{0}-{1}.root".format(ptbin[0], ptbin[1]), y_2017=False, extra_variables = [""], bins = True,tree = tree)
+                yptcut = ycuts + " && " + ptcuts
+                allcuts = " {0} && {1}".format(yptcut, mass_cuts)
+                Strip.strip_n_save(0,0, allcuts, "", saving_dir + "y_ptbins/" + particle + "_ybin_{0}-{1}_ptbin_{2}-{3}.root".format(ybin[0],ybin[1],ptbin[0],ptbin[1]), extra_variables = [""], bins = True, tree = tree)
