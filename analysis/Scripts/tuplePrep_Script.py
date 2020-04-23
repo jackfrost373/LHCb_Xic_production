967791054d935513a1bfc26b412bc712d9d1eda5
Jonastjep <jonas@tjepkema.com>
2020-04-23 20:17:11 +0200

    update tuple script

--- analysis/Scripts/tuplePrep_Script.py
+++ analysis/Scripts/tuplePrep_Script.py
@@ -102,6 +102,7 @@ def split_in_bins_n_save (root_file, saving_dir, run, mother_particle = "Lc"):
     os.mkdir(saving_dir + "ptbins/")
     os.mkdir(saving_dir + "y_ptbins/")
     
+    extra_variables = [""]
     tree = root_file
     for particle in particles:
         if particle == "Lc":
@@ -111,12 +112,12 @@ def split_in_bins_n_save (root_file, saving_dir, run, mother_particle = "Lc"):
         for ybin in ybins:
             ycuts = "lcplus_RAPIDITY >= {0} && lcplus_RAPIDITY < {1}".format(ybin[0], ybin[1])
             allcuts = " {0} && {1}".format(ycuts, mass_cuts)
-            strip_n_save(0,0, allcuts, "", saving_dir + "ybins/" + particle + "_ybin_{0}-{1}.root".format(ybin[0], ybin[1]), extra_variables = [""], bins = True, tree = tree)
+            strip_n_save(0,0, allcuts, "", saving_dir + "ybins/" + particle + "_ybin_{0}-{1}.root".format(ybin[0], ybin[1]), extra_variables, bins = True, tree = tree)
             for ptbin in ptbins:
                 ptcuts = "lcplus_PT >= {0} && lcplus_PT < {1}".format(ptbin[0], ptbin[1])
                 if (ybin[0] == 2.0):
                     allcuts = " {0} && {1}".format(ptcuts, mass_cuts)
-                    strip_n_save(0,0, allcuts, "", saving_dir + "ptbins/" + particle + "_ptbin_{0}-{1}.root".format(ptbin[0], ptbin[1]), extra_variables = [""], particle, bins = True,tree = tree)
+                    strip_n_save(0,0, allcuts, "", saving_dir + "ptbins/" + particle + "_ptbin_{0}-{1}.root".format(ptbin[0], ptbin[1]), extra_variables, particle, bins = True,tree = tree)
                 yptcut = ycuts + " && " + ptcuts
                 allcuts = " {0} && {1}".format(yptcut, mass_cuts)
                 strip_n_save(0,0, allcuts, "", saving_dir + "y_ptbins/" + particle + "_ybin_{0}-{1}_ptbin_{2}-{3}.root".format(ybin[0],ybin[1],ptbin[0],ptbin[1]), extra_variables = [""], particle, bins = True, tree = tree)
