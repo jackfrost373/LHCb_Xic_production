04fbf5fee03e9fb6fac6de89c8d7379d1a961342
Chris Pawley <pawleycj@gmail.com>
2021-03-25 12:35:12 +0100

    Various plotting fixes

---
+++ analysis/Scripts/Efficiencies/TISTOS.py
@@ -0,0 +1,104 @@
+#################
+# Doing the TISTOS method for efficiency of the trigger using sWeighted data,
+# as per LHCB-PUB-2014-039
+#################
+
+import sys
+
+sys.path.append('./')
+
+import ROOT, getopt, os
+from Imports import DATA_jobs_Dict, getDataCuts
+
+#Input dir is where the original ntuples are, sWeights contains our friend trees
+inputdir = "/dcache/bfys/jdevries/ntuples/LcAnalysis/ganga"
+sweightsdir = "/data/bfys/cpawley/sWeights/"
+
+#Years, Mag pol and part. types hardcoded
+years = [2011,2012,2015,2016,2017,2018]
+magPol= ["MagUp","MagDown"]
+particle_types=["Lc","Xic"]
+
+def main(argv):
+ 
+  ## parse the arguments from command line into arrays for us to check:
+  try:
+    opts,args=getopt.getopt(argv,"y:o:p:")
+  except getopt.GetoptError:
+    print("Incorrect Arguements used in TISTOS")
+    sys.exit(2)
+  options=[]
+  arguments=[]
+  for opt,arg in opts:
+    options.append(opt)
+    arguments.append(arg)
+    #Check inputs are viable/match what is possible
+    #What happens when we don't enter parameters? maybe we need to look for length>0 first
+  print ("Checking for parameter errors")
+  
+  particle = arguments[options.index("-p")]
+  if (particle!="Xic" and particle!="Lc" ):
+    print("Wrong particle name input to TISTOS...exiting...")
+    sys.exit()
+
+  year=int(arguments[options.index("-y")])
+  if year not in years:
+    print("Wrong year input to TISTOS...exiting...")
+    sys.exit()
+  if (year==2011 or year==2012):
+    run=1
+    particle_id=particle
+  else:
+    run=2
+    particle_id="Lc"
+
+  magpol=arguments[options.index("-o")]
+  if (magpol=="Up"):
+    magpol="MagUp"
+  elif (magpol=="Down"):
+    magpol="MagDown"
+  else:
+    print ("Wrong magnet polarity input to TISTOS...exiting...")
+    sys.exit()
+
+  
+  
+#Check sufficient parameters entered
+
+  if set(options)!=set(["-y","-o","-p"]):
+    print("Entered too few parameters in TISTOS...exiting...")
+    sys.exit()
+
+#getthedata
+
+  print("Getting the data")
+  for element in DATA_jobs_Dict:
+    if (DATA_jobs_Dict[element][0]=='{0}_{1}'.format(year,magpol)):
+      filename=("{0}2pKpiTuple.root".format(particle_id))
+      cutTotalTree=ROOT.TChain('DecayTree'.format(particle_id))
+      for job in range(DATA_jobs_Dict[element][1]):
+        if os.path.exists("{0}/{1}/{2}/{3}".format(inputdir,element,job,filename)):
+          temp_tree=ROOT.TChain('tuple_{0}2pKpi/DecayTree'.format(particle_id))
+          temp_tree.Add("{0}/{1}/{2}/{3}".format(inputdir,element,job,filename))
+          cut_temp_tree=temp_tree.CopyTree(getDataCuts(run))
+          wfile=ROOT.TFile.Open("temp.root","RECREATE")
+          wfile.cd()
+          cut_temp_tree.Write()
+          wfile.Close()
+          cutTotalTree.Add("temp.root")
+  
+  print ("The total number of entries in this nTuple is {0}".format(cutTotalTree.GetEntries()))
+  
+
+  wfile=ROOT.TFile.Open("/data/bfys/cpawley/efficiencies/TISTOS.root", "RECREATE")
+  wfile.cd()
+  cutTotalTree.Write()
+  wfile.Close()
+  os.remove("temp.root")
+
+
+  print ("The total number of entries in this nTuple is {0}".format(cutTotalTree.GetEntries()))
+  return
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
--- analysis/Scripts/Universal_comparison.py
+++ analysis/Scripts/Universal_comparison.py
@@ -1,33 +1,64 @@
 #This script is used to plot series of comparison plots over multiple variables using the plot_comparison function as defined in the Imports script. The script generated may include some lines used as indications as to whether a possible cut could be applied and the canvasses are automatically saved in a desired location
 
-import ROOT, os, Plot_comparison
+import ROOT, os, Plot_comparison, Imports
 from ROOT import TChain, TCanvas, TH1
 
-#select the user in order to have the right directory for saving the plots
-user = "Nikhef"
-
-
 #change the following string variable to select which particle you want to study
-
-#particle = "Xic"
-particle  = input("please indicate the particle (Lc or Xic): ")
+particle = "Xic"
+#particle  = input("please indicate the particle (Lc or Xic): ")
 
 #This string is a general name used for the canvas name
-#name = "Xic_vs_MC"
-name = input("please indicate the name that you would like your graphs to be saved with: ")
+name = "{0}_signal_vs_MC".format(particle)
+#name = input("please indicate the name that you would like your graphs to be saved with: ")
+
+year = "2017"
+MagPol="MagDown"
+saveEntry="1"
 
 #Here is a list of all the variables whose distribution needs to be compared
-variables_to_plot = ["lcplus_P", "lcplus_OWNPV_CHI2", "pplus_ProbNNp", "kminus_ProbNNk", "piplus_ProbNNpi", "pplus_P", "kminus_P", "piplus_P", "kminus_PIDK", "pplus_PIDp", "lcplus_IPCHI2_OWNPV", "lcplus_ETA", "lcplus_PT", "lcplus_TAU", "lcplus_PVConstrainedDTF_chi2"]
+#variables_to_plot = ["lcplus_P", "lcplus_OWNPV_CHI2", "pplus_ProbNNp", "kminus_ProbNNk", "piplus_ProbNNpi", "pplus_ETA","pplus_PT","pplus_P", "kminus_ETA","kminus_PT","kminus_P", "piplus_ETA", "piplus_PT" ,"piplus_P", "kminus_PIDK", "pplus_PIDp", "lcplus_IPCHI2_OWNPV", "lcplus_ETA", "lcplus_PT", "lcplus_TAU"]
+
+variables_to_plot = ["pplus_P","pplus_ETA","pplus_PT"]
 
 #this dictionary should contain all of the variables that want to be plotted with a line. The key should be a string of the variable and its value should be the x value at which the line should be plotted
 variables_to_plot_with_line = {}
 
-tree1 = None # tree that will be plotted in red
-tree2 =  None # tree that will be plotted in blue
+fileloc=Imports.TUPLE_PATH+"/{0}_{1}/{2}_total.root".format(year,MagPol,particle)
+f=ROOT.TFile.Open(fileloc,"READONLY")
+tree1 = f.Get("DecayTree") # tree that will be plotted in red
 
-#directory = Imports.getDirectory(user)
-directory = "/dcache/bfys/scalo"
+#tree1 =  ROOT.TChain("tuple_Lc2pKpi/DecayTree")
+#filedir="/dcache/bfys/jdevries/ntuples/LcAnalysis/ganga/91"
+#filename="Lc2pKpiTuple.root"
+#for job in range (529):
+#    tree1.Add("{0}/{1}/{2}".format(filedir, job, filename))
 
+
+#fileloc="/dcache/bfys/jtjepkem/binned_files/2017_MagDown/Lc_total.root"
+#f=ROOT.TFile.Open(fileloc,"READONLY")
+#tree1 = f.Get("DecayTree") # tree that will be plotted in blue 
+
+#tree2 =  ROOT.TChain("tuple_Lc2pKpi/DecayTree")
+#filedir="/dcache/bfys/jdevries/ntuples/LcAnalysis/ganga/145"
+#filename="MC_Lc2pKpiTuple_25103064.root"
+#for job in range (181):
+#    tree2.Add("{0}/{1}/{2}".format(filedir, job, filename))
+
+for entry in Imports.MC_jobs_Dict:
+    if Imports.MC_jobs_Dict[entry][0]==year:
+        if Imports.MC_jobs_Dict[entry][1]==MagPol:
+            if Imports.MC_jobs_Dict[entry][3]==particle:
+                saveEntry=entry
+                print ("found MC file which matches")
+                tree2 =  ROOT.TChain("tuple_Lc2pKpi/DecayTree")
+                filedir=Imports.RAW_TUPLE_PATH+entry
+                filename="MC_Lc2pKpiTuple_{0}.root".format(Imports.MC_jobs_Dict[entry][4])
+                for job in range (Imports.MC_jobs_Dict[entry][2]):
+                    tree2.Add("{0}/{1}/{2}".format(filedir, job, filename))
+                
+entry=saveEntry
+directory = "/data/bfys/jdevries/LcAnalysis_plots/MCDataComp/{0}/{1}/".format(year, MagPol)
+print("Tree1 contains {0} and Tree2 contains {1}". format(tree1.GetEntries(), tree2.GetEntries()))
 c1 = ROOT.TCanvas("c1")
 
 masshist = ROOT.TH1F("masshist", "Histogram of L_{c}^{+} mass", 300, 2200, 2600)
@@ -35,9 +66,9 @@ masshist.GetXaxis().SetTitle("M(L_{c}^{+}) [MeV/c^{2}]")
 masshist.GetYaxis().SetTitle("Number of events")
 
 #define the cuts that you want to apply based on the specific comparison you want to make
-cuts1 = Imports.getBackgroundCuts(particle)
-cuts2 = Imports.getMCCuts(particle)
-
+cuts1 = Imports.getDataCuts(2, trig=True )+ "&&" + Imports.getSWeightsCuts(particle)
+cuts2 = Imports.getMCCuts(particle, 2) + "&&" + Imports.getDataCuts(2,trig=True) + "&&" + Imports.getSWeightsCuts(particle)
+#+ 
 #dummy histograms used for the legend. Ideally, they should be removed
 histogram1 = ROOT.TH1F("masshist", "Histogram of L_{c} mass", 300, 2200, 2600)
 histogram2 = ROOT.TH1F("masshist", "Histogram of L_{c} mass", 300, 2200, 2600)
@@ -47,16 +78,22 @@ histogram1.SetLineWidth(1)
 
 histogram2.SetLineColor(9) # blue for tree2
 histogram2.SetLineWidth(1)
-extralabel1="Xic Full Background" #label for tree1 in the Legend
-extralabel2="Xic MC" #label for tree2 in the Legend
+extralabel1=" "+particle+"Data "+year+" " +MagPol #label for tree1 in the Legend
+extralabel2=" {0} MC ({1}) {2} {3})".format(particle, Imports.MC_jobs_Dict[entry][4],year,MagPol) #label for tree2 in the legend
 leg = ROOT.TLegend(0.7, 0.77, 0.89, 0.89)
 leg.SetHeader("Legend")
 leg.AddEntry(histogram1, extralabel1, "l")
 leg.AddEntry(histogram2, extralabel2, "l")
 
 for variable in variables_to_plot:
-
-    Plot_comparison.plot_comparison(variable, tree1=tree1, tree2=tree2, cuts1=cuts1, cuts2=cuts2, normalized=True, legendLocation="Right")
+    if variable == "lcplus_TAU":
+        Plot_comparison.plot_comparison(variable,tree1=tree1, tree2=tree2, cuts1=cuts1, cuts2=cuts2, normalized=True, legendLocation="Right", Override=True, xmin=0, xmax=2E-3)
+    elif variable == "lcplus_OWNPV_CHI2":
+        Plot_comparison.plot_comparison(variable,tree1=tree1, tree2=tree2, cuts1=cuts1, cuts2=cuts2, normalized=True, legendLocation="Right", Override=True, xmin=0, xmax=90)
+    elif variable == "lcplus_IPCHI2_OWNPV":
+        Plot_comparison.plot_comparison(variable,tree1=tree1, tree2=tree2, cuts1=cuts1, cuts2=cuts2, normalized=True, legendLocation="Right", Override=True, xmin=0, xmax=10)
+    else:
+        Plot_comparison.plot_comparison(variable, tree1=tree1, tree2=tree2, cuts1=cuts1, cuts2=cuts2, normalized=True, legendLocation="Right")
     leg.Draw()
     if variable in variables_to_plot_with_line:
         point = variables_to_plot_with_line[variable]
