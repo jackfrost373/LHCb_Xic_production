5a1ed6fa0d8add18970f8700bb3d65de5adbe6ad
Chris Pawley <pawleycj@gmail.com>
2021-10-20 14:33:19 +0200

    Fix Shell sintax

--- datafiles/scripts/downloadAPFiles.py
+++ datafiles/scripts/downloadAPFiles.py
@@ -30,8 +30,8 @@ for job in name_jobs:
     if len(file)>0:
       if not os.path.isdir(os.path.join(basedir,str(ganga_job),str(subjob))) : os.makedirs(os.path.join(basedir,str(ganga_job),str(subjob)))
       print("Trying to get file {0} and placing it in {1}/{2}/{3}".format(file.split('lhcb/LHCb')[1], basedir,ganga_job, subjob))
-      subprocess.run (['lb-dirac dirac-dms-get-file /lhcb/LHCb{0} -D {1}/{2}/{3}'.format (file.split('lhcb/LHCb')[1], basedir, ganga_job, subjob )])
-      subprocess.run (['cd {0}/{1}/{2} | mv *.root Lc2pKpiTuple.root'.format(basedir, ganga_job, subjob)])
+      os.system('lb-dirac dirac-dms-get-file /lhcb/LHCb{0} -D {1}/{2}/{3}'.format (file.split('lhcb/LHCb')[1], basedir, ganga_job, subjob ))
+      os.system('cd {0}/{1}/{2} | mv *.root Lc2pKpiTuple.root'.format(basedir, ganga_job, subjob))
     subjob += 1
   
   job_count += 1
