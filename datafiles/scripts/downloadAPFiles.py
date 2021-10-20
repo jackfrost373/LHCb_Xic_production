8304647196533d6d7331c8c5874c44590d282e19
Chris Pawley <pawleycj@gmail.com>
2021-10-20 16:36:45 +0200

    Make it work properly now

--- datafiles/scripts/downloadAPFiles.py
+++ datafiles/scripts/downloadAPFiles.py
@@ -29,7 +29,10 @@ for job in name_jobs:
       if not os.path.isdir(os.path.join(basedir,str(ganga_job),str(subjob))) : os.makedirs(os.path.join(basedir,str(ganga_job),str(subjob)))
       print("Trying to get file {0} and placing it in {1}/{2}/{3}".format(file.split('lhcb/LHCb')[1], basedir,ganga_job, subjob))
       os.system('lb-dirac dirac-dms-get-file /lhcb/LHCb{0} -D {1}/{2}/{3}'.format (file.split('lhcb/LHCb')[1], basedir, ganga_job, subjob ))
-      os.system('cd {0}/{1}/{2} | mv *.root Lc2pKpiTuple.root'.format(basedir, ganga_job, subjob))
+      
+      folder = (r"{0}/{1}/{2}".format(basedir, ganga_job, subjob))
+      for file_name in os.listdir(folder):
+        os.rename(folder+"/"+file_name , folder + "/{0}".format("Lc2pKpiTuple.root"))
     subjob += 1
   
   job_count += 1
