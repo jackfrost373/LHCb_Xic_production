ce03907cee0e7d333f6996af5b66868d8bc85727
Jan Hemink <j.hemink@student.maastrichtuniversity.nl>
2022-05-25 14:28:09 +0200

    parameter changes got reset

--- analysis/Scripts/MassFitting/GaussCB_CombinedFit_Dict.py
+++ analysis/Scripts/MassFitting/GaussCB_CombinedFit_Dict.py
@@ -6,14 +6,14 @@ fittingDict = {
 				"peak_range" : [2469, 2450, 2485],
 				
 				"normalisation_factor" : 0.1,
-				"exponential_normalisation_factor" : 0.05,
+				"exponential_normalisation_factor" : 0.1,
 				
 				"exponential_range" : [-0.02, -2, 0],
 				
-				"width_range" : [6, 0, 25],
+				"width_range" : [6, 4, 25],
 				
-				"cb_width_range" : [6, 1, 25],
-				"cb_alpha_range" : [24,0.0,25.0],
+				"cb_width_range" : [6, 1, 20],
+				"cb_alpha_range" : [24,1,25.0],
 				"cb_n_range" : [9.0,0.0,10.0]
 			}
 		},
@@ -23,15 +23,15 @@ fittingDict = {
 				"mass_range" : [2240, 2340],
 				"peak_range" : [2290,2260,2320],
 				
-				"normalisation_factor" : 0.005,
-				"exponential_normalisation_factor" : 0.0001,
+				"normalisation_factor" : 0.1,
+				"exponential_normalisation_factor" : 0.1,
 				
 				"exponential_range" : [-0.001, -0.5, 0.1],
 				
 				"width_range" : [6,4,20],
 				
-				"cb_width_range" : [17,8,50],
-				"cb_alpha_range" : [1.0,0.0,20.0],
+				"cb_width_range" : [17,8,20],
+				"cb_alpha_range" : [24,1,20.0],
 				"cb_n_range" : [9.0,0.0,10.0],
 			}
 		}
